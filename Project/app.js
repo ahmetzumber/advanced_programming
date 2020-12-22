9b10b06c0ef30e1c15c7996863e380022aeb8385
ahmetzumber <ahmetzumber5@gmail.com>
2020-12-23 00:11:06 +0300

    correct - incorrect answers handled

--- Project/app.js
+++ Project/app.js
@@ -1,5 +1,5 @@
 var currentQuestion = []
-let currentQuestionIndex = 0; // questions index
+var i = 0; // questions index
 let points = 0
 
 // html elements 
@@ -16,55 +16,62 @@ loadDatas();
 function if_answer_correct(id) {
     let points = 0
     points += 10
-    currentQuestionIndex++;
     setTimeout(()=>{
         document.getElementById(id).style.backgroundColor = "#70af85";
         document.getElementById(id).style.borderColor = "#70af85";
         score.innerText = points
-        
+        i++;
     },1000);   
 }
 function if_answer_incorrect(id) {
     setTimeout(()=>{
         document.getElementById(id).style.backgroundColor = "#ff4646";
         document.getElementById(id).style.borderColor = "#ff4646" 
+        for(let j = 0; j < 4; j++){
+            if (document.querySelectorAll(".options")[j].innerHTML == currentQuestion[i].correct_answer){
+                document.querySelectorAll(".options")[j].style.backgroundColor = "#70af85"
+                document.querySelectorAll(".options")[j].style.borderColor = "#70af85"
+            }
+        }
     },1000);
+    
 }
 
 
 async function loadDatas(){
-    next_button.addEventListener("click",() => {
-        currentQuestionIndex++;
-    })
-
     await fetch('https://opentdb.com/api.php?amount=50&category=18&type=multiple')
             .then(res => res.json())
             .then(data => {
                 data['results'].forEach(item => currentQuestion.push(item));
             })
-    console.log(currentQuestion[currentQuestionIndex]);
-
+    
+    currentQuestion.entries
     // test to correct answer
-    console.log("Correct answer: "+currentQuestion[currentQuestionIndex].correct_answer)
+    console.log("Correct answer: "+currentQuestion[i].correct_answer)
 
     // question text 
-    question.innerHTML = currentQuestion[currentQuestionIndex].question
+    console.log(i)
+    for(let ques of currentQuestion){
+        console.log(ques)
+    }
+    
+    question.innerHTML = currentQuestion[i].question;
 
     // random selection to correct answer
     let correct = Math.floor(Math.random() * 4) + 1;
 
     // every options has id i.e. 1,2,3,4
     if (correct == 1)
-        option_a.innerHTML = currentQuestion[currentQuestionIndex].correct_answer
+        option_a.innerHTML = currentQuestion[i].correct_answer
     else if (correct == 2)
-        option_b.innerHTML = currentQuestion[currentQuestionIndex].correct_answer
+        option_b.innerHTML = currentQuestion[i].correct_answer
     else if (correct == 3)
-        option_c.innerHTML = currentQuestion[currentQuestionIndex].correct_answer
+        option_c.innerHTML = currentQuestion[i].correct_answer
     else if (correct == 4)
-        option_d.innerHTML = currentQuestion[currentQuestionIndex].correct_answer
+        option_d.innerHTML = currentQuestion[i].correct_answer
     
     // if options has correct answer then its incorrect answer 
-    for (let incorrect of currentQuestion[currentQuestionIndex].incorrect_answers){
+    for (let incorrect of currentQuestion[i].incorrect_answers){
         if (option_a.innerHTML == '')
             option_a.innerHTML = incorrect
         else if (option_b.innerHTML == '')
@@ -76,11 +83,16 @@ async function loadDatas(){
     }
 
 }
+
+next_button.addEventListener("click", () => {
+    i += 1
+})
+
 option_a.addEventListener("click",() => {
     option_b.disabled = true
     option_c.disabled = true
     option_d.disabled = true
-    if (option_a.innerText == currentQuestion[currentQuestionIndex].correct_answer)
+    if (option_a.innerText == currentQuestion[i].correct_answer)
         return if_answer_correct("option-a")
     else
         return if_answer_incorrect("option-a")
@@ -89,7 +101,7 @@ option_b.addEventListener("click",() => {
     option_a.disabled = true
     option_c.disabled = true
     option_d.disabled = true
-    if (option_b.innerText == currentQuestion[currentQuestionIndex].correct_answer)
+    if (option_b.innerText == currentQuestion[i].correct_answer)
         return if_answer_correct("option-b")
     else
         return if_answer_incorrect("option-b")
@@ -98,7 +110,7 @@ option_c.addEventListener("click",() => {
     option_a.disabled = true
     option_b.disabled = true
     option_d.disabled = true
-    if (option_c.innerText == currentQuestion[currentQuestionIndex].correct_answer)
+    if (option_c.innerText == currentQuestion[i].correct_answer)
         return if_answer_correct("option-c")
     else
         return if_answer_incorrect("option-c")
@@ -107,7 +119,7 @@ option_d.addEventListener("click",() => {
     option_a.disabled = true
     option_b.disabled = true
     option_c.disabled = true
-    if (option_d.innerText == currentQuestion[currentQuestionIndex].correct_answer)
+    if (option_d.innerText == currentQuestion[i].correct_answer)
         return if_answer_correct("option-d")
     else
         return if_answer_incorrect("option-d")
